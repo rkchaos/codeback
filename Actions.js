b2974fd3ef8a3be4b27f19cce2d95e93d378f88f
Raj kumar <rk0346101@gmail.com>
2024-08-16 16:00:34 +0530

    comm

---
+++ Actions.js
@@ -0,0 +1,15 @@
+// All the events
+
+const ACTIONS = {
+    JOIN: "join",
+    JOINED: "joined",
+    DISCONNECTED: "disconnected",
+    CODE_CHANGE: "conde-change",
+    SYNC_CODE: "sync-code",
+    LEAVE: "leave",
+    SEND_MESSAGE: "send-message",
+    WHITEBOARD_UPDATE: 'WHITEBOARD_UPDATE',
+    AUDIO_STREAM: "AUDIO_STREAM"
+};
+
+module.exports = ACTIONS;
